0c7a0dcda93f0726d5b6dca8e242ced1a8a82dcf
FelipeRomaoDev <felipe.romao@outlook.com>
2023-05-09 21:31:15 -0300

    Arquivos aula 03 Javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+let fruta1 = "Maça";
+let fruta2 = "Pera";
+let fruta3 = "Melão";
+let fruta4 = "  Uva";
+let fruta5 = "Abacaxi";
+
+console.log(fruta5.length);
+console.log(fruta3.substring(2));
+console.log(fruta4.trim());
+let novaFruta = console.log(fruta2.replace('Pera', 'Fruta do Conde').toLowerCase());
+console.log(`${fruta1} ${fruta2} ${fruta3} ${fruta4} ${fruta5}`);
